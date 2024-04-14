6f718279090eae58d8f3884af4acf86c2151bdc7
eduferrerm <ekferrerm@gmail.com>
2024-04-15 00:10:32 +0200

    build push

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Contact from "./components/Contact";
 import Experiences from "./components/Experience";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Portfolio from "./components/Portfolio";
